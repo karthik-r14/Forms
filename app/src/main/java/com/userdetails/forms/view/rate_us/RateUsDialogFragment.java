1d7e54ec1a4c230592a78c5bd3e7866e05d428bb
Karthik R <karthikr@thoughtworks.com>
2017-04-14 16:52:54 +0530

    Now, rating gets stored in firebase database.Also, a progress bar is shown in rating dialog fragment.

--- app/src/main/java/com/userdetails/forms/view/rate_us/RateUsDialogFragment.java
+++ app/src/main/java/com/userdetails/forms/view/rate_us/RateUsDialogFragment.java
@@ -3,31 +3,46 @@ package com.userdetails.forms.view.rate_us;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.LayerDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.StringRes;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.userdetails.forms.R;
 import com.userdetails.forms.presenter.RateUsDialogPresenter;
 import com.userdetails.forms.view.feedback.FeedbackActivity;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
 import static android.widget.Toast.LENGTH_SHORT;
 
 public class RateUsDialogFragment extends DialogFragment implements RateUsView {
+    public static final String RATING = "rating";
     @BindView(R.id.ratingBar)
     RatingBar ratingBar;
     @BindView(R.id.button_layout)
@@ -38,9 +53,15 @@ public class RateUsDialogFragment extends DialogFragment implements RateUsView {
     Button notNowButton;
     @BindView(R.id.tap_stars_text)
     TextView starsText;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.rating_layout)
+    LinearLayout ratingLayout;
 
     private RateUsDialogPresenter presenter;
+    private DatabaseReference mDatabase;
     public static final String TAG = RateUsDialogFragment.class.getSimpleName();
+    private String ratingId;
 
     public static RateUsDialogFragment newInstance() {
         return new RateUsDialogFragment();
@@ -56,6 +77,8 @@ public class RateUsDialogFragment extends DialogFragment implements RateUsView {
         dialog.setContentView(view);
         dialog.setCancelable(false);
 
+        progressBar.setVisibility(VISIBLE);
+
         presenter = new RateUsDialogPresenter(this);
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
@@ -66,6 +89,24 @@ public class RateUsDialogFragment extends DialogFragment implements RateUsView {
 
         LayerDrawable stars = (LayerDrawable) ratingBar.getProgressDrawable();
         stars.getDrawable(0).setColorFilter(getResources().getColor(R.color.grey), PorterDuff.Mode.SRC_ATOP);//color of empty stars
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        ValueEventListener ratingListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ArrayList<String> ratings = (ArrayList<String>) dataSnapshot.child(RATING).getValue();
+                ratingId = Integer.toString(ratings.size());
+                progressBar.setVisibility(GONE);
+                ratingLayout.setVisibility(VISIBLE);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(TAG, "loadRating:onCancelled", databaseError.toException());
+            }
+        };
+        mDatabase.addListenerForSingleValueEvent(ratingListener);
 
         return dialog;
     }
@@ -93,11 +134,16 @@ public class RateUsDialogFragment extends DialogFragment implements RateUsView {
 
     @OnClick(R.id.submit_and_feedback_button)
     public void onSubmitButtonClick() {
+        if(!isConnectivityAvailable()) {
+            Toast.makeText(getActivity(), R.string.no_internet, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         if(submitAndFeedbackButton.getText().equals(getString(R.string.feedback))) {
             Intent intent = new Intent(getActivity(), FeedbackActivity.class);
             startActivity(intent);
         }
-
+        mDatabase.child(RATING).child(ratingId).setValue(ratingBar.getRating());
         Toast.makeText(getActivity(), R.string.thank_you_submit, LENGTH_SHORT).show();
         getDialog().dismiss();
     }
@@ -105,5 +151,11 @@ public class RateUsDialogFragment extends DialogFragment implements RateUsView {
     @OnClick(R.id.not_now)
     public void onNotNowClick() {
         getDialog().dismiss();
+    }
+
+    private boolean isConnectivityAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }

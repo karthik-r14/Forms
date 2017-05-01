6694d6dce7b7cdbcc33429a71b3657be2e183679
Karthik R <karthikr@thoughtworks.com>
2017-05-01 15:59:15 +0530

    Now, a broadcast receiver gets fired whenever the airplane mode is turned on.

--- app/src/main/java/com/userdetails/forms/receiver/MyReceiver.java
+++ app/src/main/java/com/userdetails/forms/receiver/MyReceiver.java
@@ -9,10 +9,12 @@ import android.widget.Toast;
 import com.userdetails.forms.R;
 
 public class MyReceiver extends BroadcastReceiver {
+    public static final String STATE = "state";
+
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_AIRPLANE_MODE_CHANGED)) {
-            boolean isAirplaneModeOn = intent.getBooleanExtra("state", false);
+            boolean isAirplaneModeOn = intent.getBooleanExtra(STATE, false);
 
             if (isAirplaneModeOn) {
                 Toast.makeText(context, R.string.airplane_mode_message, Toast.LENGTH_LONG).show();

bfc898cf2d842271d963cf8d5a8c4b38b914bac2
Karthik R <karthikr@thoughtworks.com>
2017-05-02 14:29:15 +0530

    Now, person model has tests.

---
+++ app/src/test/java/com/userdetails/forms/model/PersonTest.java
@@ -0,0 +1,38 @@
+package com.userdetails.forms.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+
+    private Person person;
+
+    @Before
+    public void setUp() throws Exception {
+        person = new Person("Karthik", "23", "9880443410", "karthikr1493@gmail.com");
+    }
+
+    @Test
+    public void shouldGetName() throws Exception {
+
+        assertEquals("Karthik", person.getName());
+    }
+
+    @Test
+    public void shouldGetAge() throws Exception {
+        assertEquals("23", person.getAge());
+    }
+
+    @Test
+    public void getPhoneNumber() throws Exception {
+        assertEquals("9880443410", person.getPhoneNumber());
+    }
+
+    @Test
+    public void getEmail() throws Exception {
+        assertEquals("karthikr1493@gmail.com", person.getEmail());
+    }
+
+}
